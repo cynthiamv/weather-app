651163e5d6383228b8eb29395d7cd1581faa28e5
cynthia.mvico <cynthia.mvico@gmail.com>
2021-04-07 21:42:31 -0300

    Updates

---
+++ precache-manifest.38109c01873ce04be19686e9d6e0ed35.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d914f4448c6b20e089bd0e103662df5",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "3bff9af7c54a9e7166e5",
+    "url": "/weather-app/static/js/2.63a3f54a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/weather-app/static/js/2.63a3f54a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94182dc5e6319bd9e710",
+    "url": "/weather-app/static/js/main.d93ce40d.chunk.js"
+  },
+  {
+    "revision": "d0e2e579b5155f255804",
+    "url": "/weather-app/static/js/runtime-main.e6f382d7.js"
+  },
+  {
+    "revision": "a8528a868d461f09ee29a47e4c5323c8",
+    "url": "/weather-app/static/media/Clear.a8528a86.png"
+  },
+  {
+    "revision": "11452cab98ccf2a171a36da25360ba1b",
+    "url": "/weather-app/static/media/Cloud-background.11452cab.png"
+  },
+  {
+    "revision": "c6d1cb3616b92442a99eb3acd4063a89",
+    "url": "/weather-app/static/media/LightCloud.c6d1cb36.png"
+  },
+  {
+    "revision": "b62e950be97488071f86695792d1a919",
+    "url": "/weather-app/static/media/Shower.b62e950b.png"
+  },
+  {
+    "revision": "e1202bb6505e7b4c7a9fac14eafe4162",
+    "url": "/weather-app/static/media/Sleet.e1202bb6.png"
+  }
+]);
